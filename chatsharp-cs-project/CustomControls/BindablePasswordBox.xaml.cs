c19716fa57af0694d1dc9c7b5981432dedbce42a
Jurma Daniel <d_jurma@yahoo.com>
2023-03-03 16:10:00 +0200

    LoginView & RegisterView updated with a custom 'BindablePasswordBox' control

---
+++ chatsharp-cs-project/CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MVVMEssentials.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chatsharp_cs_project.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+
+     
+        public static readonly DependencyProperty PasswordProperty=DependencyProperty.Register("Password",typeof(string),typeof(BindablePasswordBox),new PropertyMetadata(string.Empty));
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty);}
+            set { SetValue(PasswordProperty, value); }  
+        }
+        public BindablePasswordBox()
+        {
+            InitializeComponent();      
+        }
+
+        private void BindablePasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = PasswordBox.Password;
+        }
+    }
+}
