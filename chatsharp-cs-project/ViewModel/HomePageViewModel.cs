0c565a1bbefee906791afdba77db5aa908d49051
Jurma Daniel <d_jurma@yahoo.com>
2023-03-17 16:19:40 +0200

    HomePageView & HomePageViewModel added

--- chatsharp-cs-project/ViewModel/HomePageViewModel.cs
+++ chatsharp-cs-project/ViewModel/HomePageViewModel.cs
@@ -1,14 +1,24 @@
-﻿using MVVMEssentials.ViewModels;
+﻿using GalaSoft.MvvmLight.Command;
+using MVVMEssentials.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace chatsharp_cs_project.ViewModel
 {
-    class HomePageViewModel :ViewModelBase
+    class HomePageViewModel : ViewModelBase
     {
-
+        public ICommand VisitLinkedinCommand { get; set; }
+        public HomePageViewModel()
+        {
+            VisitLinkedinCommand = new RelayCommand<object>((uri) =>
+            {
+                string myUri = !uri.ToString().Contains("https://") && !uri.ToString().Contains("http://") ? "http://" + uri.ToString() : uri.ToString();
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(myUri) { UseShellExecute = true });
+            });
+        }
     }
 }
